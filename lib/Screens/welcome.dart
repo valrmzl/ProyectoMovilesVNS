917de3be843b4d71cbda01903977ea16a30e5c12
valrmzl <valeria.ramirez@iteso.mx>
2023-09-29 16:30:40 -0600

    diseño de welcome y nuevo login

---
+++ lib/Screens/welcome.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Welcome());
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:<Widget>[
+              Column(
+                children: <Widget>[
+                  Text("Bienvenido",
+                   style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30
+                   ),),
+                   SizedBox(height: 20,),
+                   Text("Texto que aun no se",
+                   textAlign: TextAlign.center,
+                   style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 15
+                   ),),
+                   
+                ],
+              ),
+
+              Container(
+                height: MediaQuery.of(context).size.height/3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/imagen2.jpeg")
+                  ),
+                ),
+              ),
+
+              //aqui es despues de la imagen principal
+
+              Column(
+
+                children: <Widget>[
+
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: (){
+                    },
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Text("Login", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+
+                    ),),
+                  ),
+                  //para separar ambps botones
+                  SizedBox(height: 20,),
+
+                  Container(
+                    
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border(
+                        bottom: BorderSide(color: Colors.black),
+                        top: BorderSide(color: Colors.black),
+                        left: BorderSide(color: Colors.black),
+                        right: BorderSide(color: Colors.black),
+
+                      ),
+
+                    ),
+                    child: MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: (){
+                    },
+                    color: Colors.yellow,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Text("Sign up", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+
+                    ),),
+                  ),
+                    
+                    
+
+                  ),
+
+
+
+                ],
+
+              ),
+    
+            ],
+            
+            ),
+        )
+    
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_vsn/Screens/login_page.dart';
+import 'package:proyecto_vsn/Screens/welcome.dart';
 
 void main() => runApp(
   MaterialApp(
     title: 'Tarea 3.5',
     theme: ThemeData(
       appBarTheme: AppBarTheme(
-        color: Color.fromARGB(255, 109, 81, 50),
+        color:Color.fromARGB(255, 47, 125, 121),
       ), 
     ),
     home: LoginPage(),
