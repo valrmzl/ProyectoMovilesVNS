f53119e4ea60107bfb10c530283958a78b439ef8
ntowfigh <ntowfigh@oracle.com>
2023-10-01 00:11:06 -0600

    arregle cosas del welcome

--- lib/Screens/welcome.dart
+++ lib/Screens/welcome.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:proyecto_vsn/Screens/create_account.dart';
 import 'package:proyecto_vsn/Screens/login_page.dart';
 
-void main() => runApp(const Welcome());
+//void main() => runApp(const Welcome());
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -11,116 +11,100 @@ class Welcome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children:<Widget>[
-              Column(
-                children: <Widget>[
-                  Text("Bienvenido",
-                   style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30
-                   ),),
-                   SizedBox(height: 20,),
-                   Text("Texto que aun no se",
-                   textAlign: TextAlign.center,
-                   style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 15
-                   ),),
-                   
-                ],
-              ),
+          child: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  "Bienvenido",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Texto que aun no se",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.grey[700], fontSize: 15),
+                ),
+              ],
+            ),
 
-              Container(
-                height: MediaQuery.of(context).size.height/3,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/imagen1.jpeg")
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/imagen1.jpeg")),
+              ),
+            ),
+
+            //aqui es despues de la imagen principal
+
+            Column(
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                   ),
                 ),
-              ),
+                //para separar ambps botones
+                SizedBox(
+                  height: 20,
+                ),
 
-              //aqui es despues de la imagen principal
-
-              Column(
-
-                children: <Widget>[
-
-                  MaterialButton(
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginPage()));
-                    },
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Colors.black
-                      ),
-                      borderRadius: BorderRadius.circular(50),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border(
+                      bottom: BorderSide(color: Colors.black),
+                      top: BorderSide(color: Colors.black),
+                      left: BorderSide(color: Colors.black),
+                      right: BorderSide(color: Colors.black),
                     ),
-                    child: Text("Login", style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18
-
-                    ),),
                   ),
-                  //para separar ambps botones
-                  SizedBox(height: 20,),
-
-                  Container(
-                    
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border(
-                        bottom: BorderSide(color: Colors.black),
-                        top: BorderSide(color: Colors.black),
-                        left: BorderSide(color: Colors.black),
-                        right: BorderSide(color: Colors.black),
-
-                      ),
-
-                    ),
-                    child: MaterialButton(
+                  child: MaterialButton(
                     minWidth: double.infinity,
                     height: 60,
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=> CreateAccount()));
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CreateAccount()));
                     },
                     color: Colors.yellow,
                     elevation: 0,
                     shape: RoundedRectangleBorder(
-                      
                       borderRadius: BorderRadius.circular(50),
                     ),
-                    child: Text("Sign up", style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18
-
-                    ),),
+                    child: Text(
+                      "Sign up",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
                   ),
-                    
-                    
-
-                  ),
-
-
-
-                ],
-
-              ),
-    
-            ],
-            
+                ),
+              ],
             ),
-        )
-    
-      ),
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,13 @@ import 'package:proyecto_vsn/Screens/welcome.dart';
 import 'package:proyecto_vsn/Screens/calendar.dart';
 
 void main() => runApp(
-  MaterialApp(
-    title: 'Tarea 3.5',
-    theme: ThemeData(
-      appBarTheme: AppBarTheme(
-        color:Color.fromARGB(255, 47, 125, 121),
-      ), 
-    ),
-    home: HomePage(),
-  ),
-);
-
-
+      MaterialApp(
+        title: 'Tarea 3.5',
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            color: Color.fromARGB(255, 47, 125, 121),
+          ),
+        ),
+        home: Welcome(),
+      ),
+    );
