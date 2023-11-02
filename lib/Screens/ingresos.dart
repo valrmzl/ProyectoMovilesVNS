718474dee8ed8d997af06ff3990e4f6088bdaf79
valrmzl <valeria.ramirez@iteso.mx>
2023-11-01 23:18:06 -0600

    bell

--- lib/Screens/ingresos.dart
+++ lib/Screens/ingresos.dart
@@ -186,6 +186,7 @@ class _IngresosState extends State<Ingresos> {
                     ]),
                   ),
                   ListView.builder(
+                  
                     shrinkWrap: true,
                     physics: NeverScrollableScrollPhysics(),
                     itemCount: items.length,
